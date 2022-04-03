75d9d1db77e2d04db2898ebc3925ff8e442792e1
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-03 15:15:00 -0300

    adicionando um observador que quando chamado executa sua ação, Padrão Observer

--- src/main/java/teste/TestesPedido.java
+++ src/main/java/teste/TestesPedido.java
@@ -4,22 +4,26 @@ import orcamento.Orcamento;
 import pedido.GeraPedido;
 import pedido.GeraPedidoHandler;
 import pedido.Pedido;
+import pedido.acao.EnviarEmailPedido;
+import pedido.acao.SalvarPedidoNoBD;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TestesPedido {
 
     public static void main(String[] args) {
         // lógica para pegar os dados de qualquer lugar (linha de comando, web, APIRest)
-        String cliente = args[0];
-        BigDecimal valorOrcamento = BigDecimal.valueOf(Long.parseLong(args[1]));
-        Integer qtdeItens = Integer.parseInt(args[3]);
+        String cliente = "args[0]";
+        BigDecimal valorOrcamento = BigDecimal.valueOf(150);
+        Integer qtdeItens = Integer.parseInt("3");
 
 
         // Gerador de pedido fica apartado e independe de onde vem os dados
         GeraPedido geradorPedido = new GeraPedido(cliente,valorOrcamento,qtdeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(/*injeção de dependencias*/);
+        GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(new SalvarPedidoNoBD(), new EnviarEmailPedido()));
         handler.execute(geradorPedido);
     }
 }
